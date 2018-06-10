fdfb001fbe5c2508c6344fdeabb710bd6424f767
Julian Fiander <julian.fiander@mangolanguages.com>
2018-06-10 00:44:00 -0400

    Gracefully handle too-high latitudes

--- app/controllers/cls_controller.rb
+++ app/controllers/cls_controller.rb
@@ -2,12 +2,17 @@ class ClsController < ApplicationController
   def sheet
     return unless sheet_params.present?
 
+    if sheet_params[:latitude].to_d > 55
+      redirect_to root_path, alert: "Latitude #{sheet_params[:latitude]} is too high – must be below 55 degrees."
+      return
+    end
+
     send_file Cls.new(sheet_params).draw, disposition: :inline
   end
 
   private
 
   def sheet_params
-    params.permit(:latitude)
+    params.permit(:latitude, :longitude, :increment)
   end
 end
